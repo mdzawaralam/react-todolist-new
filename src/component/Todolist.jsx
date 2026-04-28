1daeac89e614006b4704347db0c5e577bad44cc6
Md Zawar Alam <zawar.alam@pigeon-tech.com>
2026-04-28 15:12:05 +0530

    color have been changed

--- src/component/Todolist.jsx
+++ src/component/Todolist.jsx
@@ -23,7 +23,7 @@ function Todolist() {
 
     return (
         <>
-            <div style={{ backgroundColor: "greenyellow" }}>
+            <div style={{ backgroundColor: "red" }}>
                 <h3>This is Todolist APP</h3>
                 <form onSubmit={handleSubmit}>
                     <div>
